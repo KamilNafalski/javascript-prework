22da4f08a73cc47d3a321921cfa0087903018cdd
Kamil Nafalski <kamilnafalski@gmail.com>
2023-01-21 10:03:50 +0100

    five-task

--- js/script.js
+++ js/script.js
@@ -7,27 +7,9 @@ let computerMove = 'nieznany ruch';
 if (randomNumber == 1) {
   computerMove = 'kamień';
 }
-
-printMessage('Mój ruch to: ' + computerMove);
-
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-console.log('Wylosowana liczba to: ' + randomNumber);
-
-let computerMove = 'nieznany ruch';
-
 if (randomNumber == 2) {
   computerMove = 'papier';
-}
-
-printMessage('Mój ruch to: ' + computerMove);
-
-let randomNumber = Math.floor(Math.random() * 3 + 1);
-
-console.log('Wylosowana liczba to: ' + randomNumber);
-
-let computerMove = 'nieznany ruch';
-
+}  
 if (randomNumber == 3) {
   computerMove = 'nożyce';
 }
@@ -43,27 +25,9 @@ let playerMove = 'nieznany ruch';
 if (playerInput == '1') {
   playerMove = 'kamień';
 }
-
-printMessage('Twój ruch to: ' + playerMove);
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
 if (playerInput == '2') {
   playerMove = 'papier';
 }
-
-printMessage('Twój ruch to: ' + playerMove);
-
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
-console.log('Gracz wpisał: ' + playerInput);
-
-let playerMove = 'nieznany ruch';
-
 if (playerInput == '3') {
   playerMove = 'nożyce';
 }
