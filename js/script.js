49d344d1758213cee52cde46f0831c5202667e62
Kamil Nafalski <kamilnafalski@gmail.com>
2023-01-24 07:36:33 +0100

    seven-task-b

--- js/script.js
+++ js/script.js
@@ -20,29 +20,11 @@ function playGame(playerInput) {
 
   let argComputerMove = getMoveName(randomNumber);
 
-  /*if (randomNumber == 1) {
-    computerMove = 'kamień';
-  } else if (randomNumber == 2) {
-    computerMove = 'papier';
-  } else if (randomNumber == 3) {
-    computerMove = 'nożyce';
-  }*/
-
   printMessage('Mój ruch to: ' + argComputerMove);
-
-  /*let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');*/
 
   console.log('Gracz wpisał: ' + playerInput);
 
   let argPlayerMove = getMoveName(playerInput);
-
-  /*if (playerInput == '1') {
-    playerMove = 'kamień';
-  } else if (playerInput == '2') {
-    playerMove = 'papier';
-  } else if (playerInput == '3') {
-    playerMove = 'nożyce';
-  }*/
 
   printMessage('Twój ruch to: ' + argPlayerMove);
 
